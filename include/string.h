99b34de32cea93eaf6643dd7dba2279087b66281
vddCore <vddcore@outlook.com>
2023-01-12 11:34:40 +0100

    Added strncmp

--- include/string.h
+++ include/string.h
@@ -14,15 +14,16 @@
     #define NULL ((void *)0)
 #endif
 
-void *memchr(const void *source, int32_t target, size_t length);
-void *memcpy(void *destination, const void *source, size_t length);
-void *memmove(void *destination, const void *source, size_t length);
-void *memset(void *buffer, int32_t value, size_t length);
-int strcmp(const char *s1, const char *s2);
-char *strcat(char *destination, const char *source);
-char *strcpy(char *destination, const char *source);
-char *strncat(char *destination, const char *source, size_t length);
-char *strncpy(char *destination, const char *source, size_t length);
-size_t strlen(const char *string);
+void *memchr(const void* source, int32_t target, size_t length);
+void *memcpy(void* destination, const void* source, size_t length);
+void *memmove(void* destination, const void* source, size_t length);
+void *memset(void* buffer, int32_t value, size_t length);
+int strcmp(const char* s1, const char* s2);
+int strncmp(const char* s1, const char* s2, size_t length);
+char *strcat(char* destination, const char* source);
+char *strcpy(char* destination, const char* source);
+char *strncat(char* destination, const char* source, size_t length);
+char *strncpy(char* destination, const char* source, size_t length);
+size_t strlen(const char* string);
 
 #endif /* STRING_H_ */
--- string/strcmp.c
+++ string/strcmp.c
@@ -1,6 +1,6 @@
 #include <stdint.h>
 
-int strcmp(const char *s1, const char *s2) {
+int strcmp(const char* s1, const char* s2) {
     while(*s1 && (*s1 == *s2))
             s1++, s2++;
 
