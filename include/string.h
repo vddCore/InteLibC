6e07662131eef1f1ca18474a1a0ed1b1db9a6e70
vddCore <vddcore@outlook.com>
2023-01-12 11:34:40 +0100

    Added strcmp to the string module

--- include/string.h
+++ include/string.h
@@ -18,6 +18,7 @@ void *memchr(const void *source, int32_t target, size_t length);
 void *memcpy(void *destination, const void *source, size_t length);
 void *memmove(void *destination, const void *source, size_t length);
 void *memset(void *buffer, int32_t value, size_t length);
+int strcmp(const char *s1, const char *s2);
 char *strcat(char *destination, const char *source);
 char *strcpy(char *destination, const char *source);
 char *strncat(char *destination, const char *source, size_t length);
